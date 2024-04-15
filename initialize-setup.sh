fc2636b04cf9a9ee705826508ee3eede31dc23f9
j10c <149191550+j10c@users.noreply.github.com>
2024-04-15 17:09:54 -0500

    Added "initialize-setup.sh"

---
+++ initialize-setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Install commands and general configurations for each
+# Implement file system structure for MediaServer
+# Set folder/file permissions
+# Assign static IP address
+# Firewall changes
+
+# apt update/upgrades 
+
+### Applications:
+# curl
+# git
+# ssh server
+# docker / docker-compose
+# net-tools
+# 
+
+## Output log to .txt file on desktop of all changes.
