6b1bb059def1b73557e9282fa2729b8b17b2c14c
j10c <149191550+j10c@users.noreply.github.com>
2024-04-16 11:18:44 -0500

    update

--- filesystemlayout.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Create directories for apps (running the applications) and data (manage torrents and media)
-sudo mkdir -p /appdata/radarr /appdata/sonarr /appdata/overseerr /appdata/plex /appdata/qbittorrent /appdata/wireguard
-sudo mkdir -p /data/torrents/movies /data/torrents/shows /data/torrents/incomplete /data/media/movies /data/media/shows /data/indexer/incomplete /data/indexer/complete/movies /data/indexer/complete/shows
-
-sudo usermod -aG sudo ${USER}
--- initialize-setup.sh
+++ initialize-setup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-# Implement file system structure for MediaServer :: AND PERMISSIONS
-# Assign static IP address
+# Assign current user to Sudo group
+sudo usermod -aG sudo ${USER}
+
+# Create directories for apps (running the applications) and data (manage torrents and media)
+sudo mkdir -p /appdata/radarr /appdata/sonarr /appdata/overseerr /appdata/plex /appdata/qbittorrent /appdata/wireguard
+sudo mkdir -p /data/torrents/movies /data/torrents/shows /data/torrents/incomplete /data/media/movies /data/media/shows /data/indexer/incomplete /data/indexer/complete/movies /data/indexer/complete/shows
+# Folder permissions?
 
 # appearance=dark, dock icons size=24, terminal=favorites
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
@@ -14,30 +19,28 @@ gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell fav
 sudo apt update -y
 
 ### Applications ###
-
 # Install applications (Networking tools & Docker)
 sudo apt install curl net-tools openssh-server xrdp docker.io -y
 
-## Configure applications
-
+## Configure applications ##
 # OpenSSH Server :: firewall exception
 sudo ufw allow ssh
-# Remote desktop protocol :: On startup, firewall exception @3389/tcp
+# Remote Desktop Protocol :: On startup, firewall exception @3389/tcp
 sudo systemctl enable xrdp
 sudo systemctl start xrdp
 sudo ufw allow 3389/tcp
 sudo ufw enable -y
 sudo /etc/init.d/xrdp restart
-
-
 # Docker :: Install compose from github, make directory for it, modify permissions
 sudo systemctl enable --now docker
 mkdir -p ~/.docker/cli-plugins/
 curl -SL https://github.com/docker/compose/releases/download/v2.25.0/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
 sudo chmod +x ~/.docker/cli-plugins/docker-compose 
 sudo chmod 666 /var/run/docker.sock
+# $application :: 
+### End of Applications ###
+sudo apt upgrade -y
 
+# Assign static IP address
 
 ## Output log to .txt file on desktop of all changes.
-
-# Firewall changes
