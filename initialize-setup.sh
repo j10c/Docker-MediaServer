43fba97b9f405b7f18d550be6ed26aa9a9825bff
j10c <149191550+j10c@users.noreply.github.com>
2024-04-17 14:42:21 -0500

    updated the notes.txt

--- initialize-setup.sh
+++ initialize-setup.sh
@@ -5,7 +5,7 @@ sudo usermod -aG sudo ${USER}
 
 # Create directories for apps (running the applications) and data (manage torrents and media)
 sudo mkdir -p /appdata/radarr /appdata/sonarr /appdata/overseerr /appdata/plex /appdata/qbittorrent /appdata/wireguard
-sudo mkdir -p /data/torrents/movies /data/torrents/shows /data/torrents/incomplete /data/media/movies /data/media/shows /data/indexer/incomplete /data/indexer/complete/movies /data/indexer/complete/shows
+sudo mkdir -p /data/torrents/movies /data/torrents/shows /data/media/movies /data/media/shows /data/indexer/incomplete /data/indexer/complete/movies /data/indexer/complete/shows
 # Folder permissions?
 
 # appearance=dark, dock icons size=24, terminal=favorites
@@ -20,7 +20,7 @@ sudo apt update -y
 
 ### Applications ###
 # Install applications (Networking tools & Docker)
-sudo apt install curl net-tools openssh-server xrdp docker.io -y
+sudo apt install curl net-tools openssh-server xrdp docker.ce -y
 
 ## Configure applications ##
 # OpenSSH Server :: firewall exception
@@ -37,10 +37,9 @@ mkdir -p ~/.docker/cli-plugins/
 curl -SL https://github.com/docker/compose/releases/download/v2.25.0/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
 sudo chmod +x ~/.docker/cli-plugins/docker-compose 
 sudo chmod 666 /var/run/docker.sock
-# $application :: 
+# Portainer ::
+# volume?
+docker pull portainer/portainer-ce
+
 ### End of Applications ###
 sudo apt upgrade -y
-
-# Assign static IP address
-
-## Output log to .txt file on desktop of all changes.
